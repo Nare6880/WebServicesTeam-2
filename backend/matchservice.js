a4d2ed8a8d4a65b5731a5f7c54f0cfac7ecd975d
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2023-03-15 14:45:20 -0400

    Add get All PlayersFunction

--- backend/matchservice.js
+++ backend/matchservice.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const firebaseAdmin = require("firebase-admin");
+const serviceAccount = require("../ServiceAccountKey.json");
+firebaseAdmin.initializeApp({
+	credential: firebaseAdmin.credential.cert(serviceAccount),
+});
+const database = firebaseAdmin.firestore();
 const app = express();
 app.use(express.json());
 app.use(function (req, res, next) {
@@ -9,7 +15,26 @@ app.use(function (req, res, next) {
 	);
 	next();
 });
-app.get("/", (req, res) => {
-	res.send("hello world");
+let playerData;
+// /getPlayer().then(res =>{
+// 	console.log(result.body);
+// 	const player = JSON.parse(result.body);
+// 	playerData = {
+// 		Username: player.UserName,
+// 		ToxicityElo: player.ToxicityElo,
+// 		SkillElo: player.ToxicityElo,
+// 	};
+
+// })
+//database.collection("players").add({});
+getPlayers().then((res) => {
+	console.log(res);
 });
+async function getPlayers() {
+	const playersRef = await database.collection("players").get();
+	return playersRef.docs.map((doc) => {
+		return { id: doc.id, data: doc.data() };
+	});
+}
+
 app.listen(4000, () => console.log("listening on 4000"));
