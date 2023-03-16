8d11e9e2f1c9c2c39fe119803df4cc0d927b5e63
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2023-03-16 11:55:55 -0400

    push users functionality

--- backend/matchservice.js
+++ backend/matchservice.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const firebaseAdmin = require("firebase-admin");
+const gametagStore = require("../GamerTags.json");
+const gamertagsArr = gametagStore.Gamertags;
 const serviceAccount = require("../ServiceAccountKey.json");
 firebaseAdmin.initializeApp({
 	credential: firebaseAdmin.credential.cert(serviceAccount),
@@ -35,6 +37,35 @@ async function getPlayers() {
 	return playersRef.docs.map((doc) => {
 		return { id: doc.id, data: doc.data() };
 	});
+}
+function generatePlayers(nPlayers) {
+	baseElo = 1000;
+	arr = [];
+	for (i = 0; i < nPlayers; i++) {
+		arr.push({
+			ActualSkillElo: baseElo * (randn_bm() / 0.5),
+			SkillElo: baseElo,
+			ActualToxicityElo: baseElo * (randn_bm() / 0.5),
+			ToxicityElo: baseElo,
+			UserName: gamertagsArr[Math.floor(Math.random() * gamertagsArr.length)],
+		});
+	}
+	return arr;
+}
+app.get("/", (req, res) => {
+	res.send(generatePlayers(2000));
+});
+console.log(generatePlayers(400), "lmao");
+//generate random values on a bellcurve taken from https://stackoverflow.com/questions/25582882/javascript-math-random-normal-distribution-gaussian-bell-curve
+function randn_bm() {
+	let u = 0,
+		v = 0;
+	while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+	while (v === 0) v = Math.random();
+	let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+	num = num / 10.0 + 0.5; // Translate to 0 -> 1
+	if (num > 1 || num < 0) return randn_bm(); // resample between 0 and 1
+	return num;
 }
 
 app.listen(4000, () => console.log("listening on 4000"));
